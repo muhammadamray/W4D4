5ffe7fc2fa846e2fd8204e3ccec26a55759cf22e
muhammadamray <muhammadamray@gmail.com>
2023-07-20 16:09:34 -0400

    fixed files

--- practice-for-ch-ruby-anagrams-long-practice-main/problems.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/problems.rb
@@ -6,3 +6,21 @@ def anaagram?(str, str2)
 
     new_arr.include?(str2)
 end 
+
+def second_anagram?(str1, str2)
+    str2_arr = str2.split("")
+    str1.each_char do |char|
+      if !str2_arr.find_index(char).nil?
+        idx = str2_arr.find_index(char)
+        str2_arr.delete(idx)
+      elsif str2_arr.empty?
+        return true
+      else
+        false
+      end
+  
+    end
+  
+  
+  end
+  
